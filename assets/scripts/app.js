b1d547746ec80bb6c846eed64d3fa50c91b984ef
Thiago Luiz <thiagoluix21@gmail.com>
2023-11-28 21:11:49 -0300

    perf: :zap: Improved code performance
    A function was created to generate the texts of each property generated by the api and error handling was also carried out in case of an invalid zip code.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,18 @@
 const inputSearch = $("#inputSearch");
 const buttonSearch = $("#buttonSearch");
 
+function createLine(tag, data) {
+  const line = document.createElement(tag);
+  let text = document.createTextNode(data);
+
+  line.append(text);
+  $(".cep-details").append(line);
+}
+
 buttonSearch.click((event) => {
   event.preventDefault();
+
+  $(".cep-details").empty();
 
   let zipCode = inputSearch.val();
 
@@ -15,15 +25,27 @@ buttonSearch.click((event) => {
   fetch(`https://viacep.com.br/ws/${zipCode}/json/`)
     .then((response) => response.json())
     .then((data) => {
-      $(".cep-container").css({ display: "flex" });
+      if (data.erro) {
+        throw new Error("CEP inválido");
+      }
 
+      $(".cep-container").css({
+        display: "flex",
+        "justify-content": "space-between",
+      });
+
+      $(".cep-view").show();
       $("#cepNumber").html(data.cep);
-      $("#cepCity").html(data.localidade);
-      $("#cepUf").html(data.uf);
-      $("#cepDdd").html(data.ddd);
-      $("#cepAddress").html(data.logradouro);
-      $("#cepNeighborhood").html(data.bairro);
-
-      inputSearch.val("");
+      createLine("span", `${data.localidade} / ${data.uf}`);
+      createLine("span", data.ddd);
+      createLine("span", data.logradouro);
+      createLine("span", data.bairro);
+    })
+    .catch((error) => {
+      $(".cep-container").css({ "justify-content": "center" });
+      $(".cep-view").hide();
+      createLine("Ops, algo deu errado!");
     });
+
+  inputSearch.val("");
 });
